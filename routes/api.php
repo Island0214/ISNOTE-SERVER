f74085436ada7c6b72c66f410f3ffef179c898ea
Island0214 <549378813@qq.com>
2017-11-16 13:43:56 +0800

    <fix> : <修改一些配置>

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,19 +12,31 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
 
 $api = app('Dingo\Api\Routing\Router');
-
 $api->version('v1', function ($api) {
     $api->group(['namespace' => 'App\Api\Controllers'], function ($api) {
-        $api->post('user/login','AuthController@authenticate');
-        $api->post('user/register','AuthController@register');
+        $api->post('user/login', 'AuthController@authenticate');
+        $api->post('user/register', 'AuthController@register');
+        $api->group(['middleware' => 'jwt.auth'], function ($api) {
+            $api->get('user/me', 'AuthController@getAuthenticatedUser');
+        });
     });
-});
-
-$api->group(['middleware'=>'jwt.auth'],function ($api){
-    $api->get('user/me','AuthController@getAuthenticatedUser');
 });
